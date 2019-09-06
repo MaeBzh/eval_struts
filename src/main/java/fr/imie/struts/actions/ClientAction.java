7f36eb1aa09e68bdf24634ed85cd339ed1ff4e13
Maelenn Picaud <Maelenn.picaud@gmail.com>
2019-09-06 09:40:10 +0200

    first commit

---
+++ src/main/java/fr/imie/struts/actions/ClientAction.java
@@ -0,0 +1,19 @@
+package fr.imie.struts.actions;
+
+import fr.imie.struts.javaBeans.Client;
+
+public class ClientAction {
+
+    private Client client;
+
+    public ClientAction() {
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+}
---
+++ src/main/java/fr/imie/struts/javaBeans/Client.java
@@ -0,0 +1,61 @@
+package fr.imie.struts.javaBeans;
+
+public class Client {
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String password;
+    private String email;
+    private String adress;
+
+    public Client() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+}
